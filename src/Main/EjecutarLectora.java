4a2159d64a9a2c403ed6bad72f69cfb17bec8885
Jhon Moreira <jonh.m.10@gmail.com>
2018-05-10 09:32:58 -0600

    Ejecucion externa del formulario

---
+++ src/Main/EjecutarLectora.java
@@ -0,0 +1,18 @@
+package Main;
+
+import Formularios.CapturaHuella;
+
+/**
+ *
+ * @author Jhon
+ */
+public class EjecutarLectora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new CapturaHuella().setVisible(true);
+    }
+    
+}
